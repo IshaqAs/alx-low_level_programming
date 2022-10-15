a7f95e2e61940e97315342d75b9f7449eb184974
IshaqAs <IshaqAs>
2022-10-14 21:50:57 -0700

    my first line of C

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *@puts - print function
+ *Return: always 0 (success)
+*/
+
+int main(void)
+{
+	("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
