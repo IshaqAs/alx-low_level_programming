29af307f2f7ae373f51bf32b43f242be71ae21a6
IshaqAs <IshaqAs>
2022-10-17 18:49:02 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <std.io>
+
+
+/**
+ * main - print the lowercase alphabet in reverse
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'z'; letter >= 'a'; letter--)
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
