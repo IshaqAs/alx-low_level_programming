64d040ccfb1543fc3de46c124c8e954563a0b00d
IshaqAs <IshaqAs>
2022-10-17 18:29:37 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+/**
+ * main - print
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+
+	for (letter = 'A'; letter <= 'z'; letter++)
+		putcahr(letter);
+
+	putchar('\n');
+
+	return (0);
+}
